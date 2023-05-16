0a08fde981325df11670903cda65c4fcd1456275
m94soso <m94k@hotmail.com>
2023-05-16 12:24:11 +0400

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("this is log")
+console.log(typeof numb)
+var numb =25;
+console.log(typeof numb)
+numb=90;
+console.log(typeof(typeof numb))
+numb=null;
+console.log(numb)
+
+var ID =null;
+var pass;
+console.log(ID == pass);
+
+var loggon =false;
+console.log(loggon);
+var myname ="mohamed";
+console.log(myname);
+var myage=29;
+console.log(myage);
+
+console.log(myname.indexOf("A"));
+console.log(myname.charAt(0));
